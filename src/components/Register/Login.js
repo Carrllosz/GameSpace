f7a23943559d1f9f3aab9dc5d36457911f712f83
joaocarlos <joaocar2003@gmail.com>
2024-01-18 17:14:17 -0300

    first commit

--- src/components/Register/Login.js
+++ src/components/Register/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import {
   Title,
   LoginContainer,
@@ -11,18 +11,35 @@ import {
   RouteSingup,
 } from './LoginElements';
 import { Link } from 'react-router-dom'; 
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      axios.post('http://localhost:3001/login', { email, password})
+      .then(result => {
+        console.log(result)
+        if(result.data === "Sucesso") {
+          navigate('/')
+        } 
+      })
+      .catch(err=> console.log(err))
+  }
   return (
     <LoginContainer>
       <LoginFormContainer>
         <Title>Login</Title>
-        <LoginForm>
-          <FormLabel htmlFor="e-mail">E-mail</FormLabel>
-          <FormInput type="text" id="e-mail" placeholder="Digite seu e-mail" />
+        <LoginForm onSubmit={handleSubmit}>
+          <FormLabel htmlFor="email">E-mail</FormLabel>
+          <FormInput type="text" id="email" placeholder='Digite seu e-mail' onChange={(e) => setEmail(e.target.value)}/>
           <FormLabel htmlFor="password">Senha</FormLabel>
-          <FormInput type="password" id="password" placeholder="Digite sua senha" />
-
+          <FormInput type="password" id="password" placeholder='Digite sua senha' onChange={(e) => setPassword(e.target.value)}/>
           <LoginButton type="submit">Login</LoginButton>
           <RouteSingup>Não tem uma conta? <Link to="/Signup"> Clique aqui</Link></RouteSingup>
         </LoginForm>
--- src/components/Register/SignUp.js
+++ src/components/Register/SignUp.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SignContainer, SingupForm, FormInput, FormLabel, SingupButton, Title, SignFormContainer, ImageContainer,RouteLogin } from './SignupElements';
-import { Link } from 'react-router-dom'; 
+import {  Link } from 'react-router-dom'; 
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3001/register', {name, email, password})
+        .then(result => {console.log(result)
+            navigate('/login')
+        })
+        .catch(err=> console.log(err))
+    }
+
+
     return (
         <SignContainer>
             <SignFormContainer>
                 <Title>Sign Up</Title>
-                    <SingupForm>
+                    <SingupForm onSubmit={handleSubmit}>
                         <FormLabel htmlFor="nome">Nome</FormLabel>
-                        <FormInput type="text" id="nome" placeholder='Digite seu nome'/>
-                        <FormLabel htmlFor="e-mail">E-mail</FormLabel>
-                        <FormInput type="text" id="e-mail" placeholder='Digite seu e-mail'/>
+                        <FormInput type="text" id="nome" placeholder='Digite seu nome' onChange={(e) => setName(e.target.value)}/>
+                        <FormLabel htmlFor="email">E-mail</FormLabel>
+                        <FormInput type="text" id="email" placeholder='Digite seu e-mail' onChange={(e) => setEmail(e.target.value)}/>
                         <FormLabel htmlFor="password">Senha</FormLabel>
-                        <FormInput type="text" id="password" placeholder='Digite sua senha'/>
-                        <FormLabel htmlFor="checkpassword">Repita a senha</FormLabel>
-                        <FormInput type="text" id="checkpassword" placeholder='Digite sua senha novamente'/>
-
-                        <SingupButton type="submit">Sing Up</SingupButton>
+                        <FormInput type="password" id="password" placeholder='Digite sua senha' onChange={(e) => setPassword(e.target.value)}/>
+                        <SingupButton type="submit" >Sing Up</SingupButton>
                         <RouteLogin>Já tem uma conta? <Link to="/login"> Clique aqui</Link></RouteLogin>
                     </SingupForm>
             </SignFormContainer>
